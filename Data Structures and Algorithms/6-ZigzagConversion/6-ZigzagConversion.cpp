f837acfd34469b5908442bc30afcd977c6b1fe5e
Vishvaa K <kvishvaa6@gmail.com>
2026-06-04 18:38:09 +0530

    Create 6-ZigzagConversion

---
+++ Data Structures and Algorithms/6-ZigzagConversion/6-ZigzagConversion.cpp
@@ -0,0 +1,31 @@
+// Last updated: 6/4/2026, 6:38:08 PM
+class Solution {
+public:
+    string convert(string s, int numRows) {
+         if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+
+        int idx = 0, d = 1;
+        vector<vector<char>> rows(numRows);
+
+        for (char c : s) {
+            rows[idx].push_back(c);
+            if (idx == 0) {
+                d = 1;
+            } else if (idx == numRows - 1) {
+                d = -1;
+            }
+            idx += d;
+        }
+
+        string result;
+        for (const auto& row : rows) {
+            for (char c : row) {
+                result += c;
+            }
+        }
+
+        return result;
+    }
+};
