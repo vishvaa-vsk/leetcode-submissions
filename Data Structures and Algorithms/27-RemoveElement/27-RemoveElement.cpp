b69f8b0e2f6b2fe42020ddfff78e54174d767eb5
Vishvaa K <kvishvaa6@gmail.com>
2026-06-04 18:37:26 +0530

    Create 27-RemoveElement

---
+++ Data Structures and Algorithms/27-RemoveElement/27-RemoveElement.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/4/2026, 6:37:25 PM
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k; 
+    }
+};
