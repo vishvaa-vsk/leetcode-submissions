7c84bf5537f13eb3519a09d3c036fbc304caa74e
Vishvaa K <kvishvaa6@gmail.com>
2026-06-04 18:36:21 +0530

    Create 57-InsertInterval

---
+++ Data Structures and Algorithms/57-InsertInterval/57-InsertInterval.cpp
@@ -0,0 +1,18 @@
+// Last updated: 6/4/2026, 6:36:21 PM
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (res.back()[1] >= intervals[i][0]) {
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            } else {
+                res.push_back(intervals[i]);
+            }
+        }
+        return res;        
+    }
+};
