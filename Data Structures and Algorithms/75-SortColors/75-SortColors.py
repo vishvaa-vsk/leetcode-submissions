3f6e8926b9a85c189e1c86f2b8b818bd3f479890
Vishvaa K <kvishvaa6@gmail.com>
2026-06-07 23:25:58 +0530

    Create 75-SortColors

---
+++ Data Structures and Algorithms/75-SortColors/75-SortColors.py
@@ -0,0 +1,22 @@
+# Last updated: 6/7/2026, 11:25:58 PM
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        low, mid = 0, 0
+        high = len(nums) - 1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            elif nums[mid] == 2:
+                nums[high], nums[mid] = nums[mid], nums[high]
+                high -= 1
+        return
+
+        
